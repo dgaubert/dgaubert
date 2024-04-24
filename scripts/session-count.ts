62e09d2e6aa1db1522a0b045beb6d5859fea8de6
Daniel G Aubert <danielgarciaaubert@gmail.com>
2024-04-24 17:10:24 +0200

    chore: add script to count live site sessions

---
+++ scripts/session-count.ts
@@ -0,0 +1,18 @@
+const siteSessions = [];
+
+// 79638f4a-856e-4f8f-8a71-e734b09ae10f
+const databaseId = Deno.env.get("DENO_KV_DATABASE_ID")
+
+if (!databaseId) {
+  throw new Error("Missing DENO_KV_DATABASE_ID env variable")
+}
+
+const kv = await Deno.openKv(`https://api.deno.com/databases/${databaseId}/connect`);
+
+const iter = kv.list<string>({ prefix: ["site_sessions"] });
+
+for await (const res of iter) {
+  siteSessions.push(res);
+}
+
+console.log(`Number of active sessions: ${siteSessions.length}`)
